ca0362ed577c60da2e8a57cd2894b15f5cc0a68f
liberodark <liberodark@gmail.com>
2025-03-14 19:53:42 +0100

    0.4.0

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    cargo-audit
+    rustfmt
+    rust-analyzer
+  ];
+
+  shellHook = ''
+    rustfmt --edition 2024 src/main.rs
+    cargo audit
+  '';
+
+  RUST_BACKTRACE = 1;
+}
