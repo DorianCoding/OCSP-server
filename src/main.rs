6d2a6296c46c162fbe35f8656a1bba734f993470
DorianCoding <108593662+DorianCoding@users.noreply.github.com>
2025-03-16 11:12:35 +0100

    small patches

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate rocket;
 use chrono::{self, NaiveDateTime, Timelike};
 use chrono::{DateTime, Datelike, FixedOffset};
-use clap::{crate_authors, Parser};
+use clap::Parser;
 use config_file::FromConfigFile;
 use mysql::prelude::Queryable;
 use mysql::*;
@@ -22,10 +22,10 @@ use ocsp::{
 use pem::parse;
 use ring::digest::SHA1_FOR_LEGACY_USE_ONLY;
 use ring::{rand, signature};
-use rocket::http::ContentType;
 use rocket::State;
-use rocket::{data::ToByteUnit, Data};
-use serde::Deserialize;
+use rocket::http::ContentType;
+use rocket::{Data, data::ToByteUnit};
+use r#struct::*;
 use std::error::Error;
 use std::fs;
 use std::io;
@@ -35,115 +35,7 @@ use std::time::Duration;
 use x509_parser::oid_registry::OID_X509_EXT_AUTHORITY_KEY_IDENTIFIER;
 use x509_parser::prelude::ParsedExtension;
 use zeroize::Zeroize;
-#[derive(Parser, Debug)]
-#[clap(
-    author = crate_authors!("\n"),
-    before_help = "This script listens and answers to OCSP requests/response.",
-    after_help = "A config file is required for the script to work.",
-    help_template = "\
-    {name} {version}
-    Authors: {author-section}
-    {before-help}
-    About: {about-with-newline}
-    {usage-heading} {usage}
-
-    {all-args}{after-help}
-    "
-)]
-#[command(version, author, about, long_about = None)]
-struct Cli {
-    #[arg(default_value = "config.toml")]
-    config_path: String,
-}
-
-const CACHEFORMAT: &str = "%Y-%m-%d-%H-%M-%S";
-const DEFAULT_PORT: u16 = 9000;
-const DEFAULT_TIMEOUT: u8 = 5;
-// In a real application, this would likely be more complex.
-#[derive(Debug)]
-struct Config {
-    issuer_hash: (Vec<u8>, Vec<u8>, bool),
-    cert: Bytes,
-    revocextended: bool,
-    time: u8,
-    //issuer_name_hash: u32,
-    rsakey: ring::signature::RsaKeyPair,
-    cachedays: u16,
-    caching: bool,
-    dbip: Option<String>,
-    dbuser: String,
-    dbpassword: String,
-    dbname: String,
-    cachefolder: String,
-}
-impl Drop for Config {
-    fn drop(&mut self) {
-        self.dbip.zeroize();
-        self.dbuser.zeroize();
-        self.dbpassword.zeroize();
-        self.dbname.zeroize();
-    }
-}
-#[derive(Deserialize)]
-struct Fileconfig {
-    cachedays: u16,
-    caching: Option<bool>,
-    revocextended: Option<bool>,
-    dbip: Option<String>,
-    port: Option<u16>,
-    timeout: Option<u8>,
-    dbuser: String,
-    dbpassword: String,
-    dbname: String,
-    cachefolder: String,
-    itkey: String,
-    itcert: String,
-}
-#[derive(Debug, PartialEq, Clone)]
-struct Certinfo {
-    status: String,
-    revocation_time: Option<mysql::Value>,
-    revocation_reason: Option<String>,
-}
-#[test]
-fn testresponse() {
-    use ring::rand::SecureRandom;
-    use ring::signature::KeyPair;
-    use std::time::Instant;
-    println!("Generating key, may take a while...");
-    let rng = rand::SystemRandom::new();
-    let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
-
-    // Normally the application would store the PKCS#8 file persistently. Later
-    // it would read the PKCS#8 file from persistent storage to use it.
-
-    let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
-
-    println!("Done.");
-    let mut tosign = [0u8; 3000];
-    println!("Generating random");
-    rng.fill(&mut tosign).unwrap();
-    println!("Done!");
-    let time = Instant::now();
-    for i in 0..100 {
-        if i % 10 == 0 {
-            rng.fill(&mut tosign).unwrap();
-        }
-        let sig = key_pair.sign(&tosign);
-        // Normally an application would extract the bytes of the signature and
-        // send them in a protocol message to the peer(s). Here we just get the
-        // public key key directly from the key pair.
-        let peer_public_key_bytes = key_pair.public_key().as_ref();
-
-        // Verify the signature of the message using the public key. Normally the
-        // verifier of the message would parse the inputs to this code out of the
-        // protocol message(s) sent by the signer.
-        let peer_public_key =
-            signature::UnparsedPublicKey::new(&signature::ED25519, peer_public_key_bytes);
-        peer_public_key.verify(&tosign, sig.as_ref()).unwrap();
-    }
-    println!("Elapsed time : {:.6} ms", time.elapsed().as_millis());
-}
+mod r#struct;
 fn signresponse(
     issuer_hash: &[u8],
     private_key: &ring::rsa::KeyPair,
@@ -485,7 +377,9 @@ async fn upload(
                 }
             }
             (.., true, false) if !state.issuer_hash.2 => {
-                error!("Certificate used has not OCSP signing extended key usage and cannot sign OCSP!");
+                error!(
+                    "Certificate used has not OCSP signing extended key usage and cannot sign OCSP!"
+                );
                 return Ok((
                     custom,
                     signnonvalidresponse(OcspRespStatus::TryLater).unwrap(),
@@ -591,34 +485,6 @@ fn pem_to_der(pem_str: &str) -> Vec<u8> {
         }
     }
 }
-#[test]
-#[should_panic(expected = "called `Result::unwrap()` on an `Err` value: KeyRejected(\"InvalidEncoding\")")]
-fn checkconfigfake() {
-    let cli = Cli::parse();
-
-    let config_path = &cli.config_path;
-
-    if !Path::new(config_path).exists() {
-        panic!("Config file not found at: {}", config_path);
-    }
-
-    let mut config = match Fileconfig::from_config_file(config_path) {
-        Ok(config) => config,
-        Err(e) => {
-            panic!("Error reading config file at {}: {}", config_path, e);
-        }
-    };
-    config.itkey = String::from("test_files/key.pem");
-    //For an unknown reason, subject key identifier is not equal to SHA1 hash key so it is used instead.
-    //let issuer_name_hash = certpem.subject_name_hash();
-    let mut key = fs::read(&config.itkey).unwrap();
-    let _rsakey = getprivatekey(&key).unwrap();
-    key.zeroize();
-}
-#[test]
-fn checkconfig() {
-    rocket();
-}
 #[launch]
 fn rocket() -> _ {
     let cli = Cli::parse();
@@ -649,7 +515,9 @@ fn rocket() -> _ {
         .unwrap()
         .is_some_and(|f| f.value.ocsp_signing || f.value.any);
     if !isocsp {
-        eprintln!("Your certificate does not have OCSP signing extended key usage. If it is not the issuer, the application won't sign the response.")
+        eprintln!(
+            "Your certificate does not have OCSP signing extended key usage. If it is not the issuer, the application won't sign the response."
+        )
     }
     //let subjectkey = format!("{:x}", issuerkey).to_uppercase().replace(":", "");
     let parsed = certpem
@@ -684,13 +552,13 @@ fn rocket() -> _ {
         time: config.timeout.unwrap_or(DEFAULT_TIMEOUT),
         //issuer_name_hash,
         rsakey,
-        cachefolder: config.cachefolder,
+        cachefolder: config.cachefolder.clone(),
         caching: config.caching.unwrap_or(true),
         cachedays: config.cachedays,
-        dbip: config.dbip,
-        dbuser: config.dbuser,
-        dbpassword: config.dbpassword,
-        dbname: config.dbname,
+        dbip: config.dbip.clone(),
+        dbuser: config.dbuser.clone(),
+        dbpassword: config.dbpassword.clone(),
+        dbname: config.dbname.clone(),
     };
     let path = Path::new(config.cachefolder.as_str());
     if !path.exists() {
@@ -701,4 +569,84 @@ fn rocket() -> _ {
         .mount("/", routes![upload])
         .mount("/", routes![upload2])
         .manage(config)
+}
+#[cfg(test)]
+mod tests {
+    use ring::rand::SecureRandom;
+    use ring::signature::KeyPair;
+    use zeroize::Zeroize;
+    use std::{fs, time::Instant};
+        use std::path::Path;
+        use crate::rocket;
+    use clap::Parser;
+    use crate::{getprivatekey, Fileconfig};
+    use ring::{rand, signature};
+    use config_file::FromConfigFile;
+    use crate::Cli;
+#[test]
+fn testresponse() {
+    println!("Generating key, may take a while...");
+    let rng = rand::SystemRandom::new();
+    let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
+
+    // Normally the application would store the PKCS#8 file persistently. Later
+    // it would read the PKCS#8 file from persistent storage to use it.
+
+    let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+
+    println!("Done.");
+    let mut tosign = [0u8; 3000];
+    println!("Generating random");
+    rng.fill(&mut tosign).unwrap();
+    println!("Done!");
+    let time = Instant::now();
+    for i in 0..100 {
+        if i % 10 == 0 {
+            rng.fill(&mut tosign).unwrap();
+        }
+        let sig = key_pair.sign(&tosign);
+        // Normally an application would extract the bytes of the signature and
+        // send them in a protocol message to the peer(s). Here we just get the
+        // public key key directly from the key pair.
+        let peer_public_key_bytes = key_pair.public_key().as_ref();
+
+        // Verify the signature of the message using the public key. Normally the
+        // verifier of the message would parse the inputs to this code out of the
+        // protocol message(s) sent by the signer.
+        let peer_public_key =
+            signature::UnparsedPublicKey::new(&signature::ED25519, peer_public_key_bytes);
+        peer_public_key.verify(&tosign, sig.as_ref()).unwrap();
+    }
+    println!("Elapsed time : {:.6} ms", time.elapsed().as_millis());
+}
+#[test]
+#[should_panic(
+    expected = "called `Result::unwrap()` on an `Err` value: KeyRejected(\"InvalidEncoding\")"
+)]
+fn checkconfigfake() {
+    let cli = Cli::parse();
+
+    let config_path = &cli.config_path;
+
+    if !Path::new(config_path).exists() {
+        panic!("Config file not found at: {}", config_path);
+    }
+
+    let mut config = match Fileconfig::from_config_file(config_path) {
+        Ok(config) => config,
+        Err(e) => {
+            panic!("Error reading config file at {}: {}", config_path, e);
+        }
+    };
+    config.itkey = String::from("test_files/key.pem");
+    //For an unknown reason, subject key identifier is not equal to SHA1 hash key so it is used instead.
+    //let issuer_name_hash = certpem.subject_name_hash();
+    let mut key = fs::read(&config.itkey).unwrap();
+    let _rsakey = getprivatekey(&key).unwrap();
+    key.zeroize();
+}
+#[test]
+fn checkconfig() {
+    rocket();
+}
 }
